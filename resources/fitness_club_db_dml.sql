90c17646828e4908c26e9ec6ca6674b7cdd4cf1c
avlebedev <avlebedev@fil-it.ru>
2023-03-24 19:39:01 +0300

    DML senond commit

--- resources/fitness_club_db_dml.sql
+++ resources/fitness_club_db_dml.sql
@@ -59,5 +59,35 @@ VALUES (1, 1, 3),
        (7, 3, 2),
        (8, 4, 1);
 
--- INSERT INTO traning(coach_id, sport_id, client_id, price, shedule_id)
--- VALUES ()
+INSERT INTO traning(coach_id, sport_id, client_id, price, shedule_id)
+    VALUES (1, 3, 21, 2400 * (select duration_hours from schedule where schedule.coach_id = 1), 1),
+       (1, 3, 25, 2400 * (select duration_hours from schedule where schedule.coach_id = 1), 1),
+       (1, 3, 27, 2400 * (select duration_hours from schedule where schedule.coach_id = 1), 1),
+       (1, 1, 29, 2400 * (select duration_hours from schedule where schedule.coach_id = 1), 1),
+       (1, 1, 30, 2400 * (select duration_hours from schedule where schedule.coach_id = 1), 1),
+       (1, 1, 32, 2400 * (select duration_hours from schedule where schedule.coach_id = 1), 1),
+       (1, 1, 33, 2400 * (select duration_hours from schedule where schedule.coach_id = 1), 1),
+       (1, 1, 36, 2400 * (select duration_hours from schedule where schedule.coach_id = 1), 1),
+       (1, 1, 40, 2400 * (select duration_hours from schedule where schedule.coach_id = 1), 1);
+VALUES (2, 4, 40, 1500 * (select duration_hours from schedule where schedule.coach_id = 2), 2),
+ (2, 4, 39, 1500 * (select duration_hours from schedule where schedule.coach_id = 2), 2),
+ (2, 4, 37, 1500 * (select duration_hours from schedule where schedule.coach_id = 2), 2),
+ (2, 4, 28, 1500 * (select duration_hours from schedule where schedule.coach_id = 2), 2),
+ (2, 4, 24, 1500 * (select duration_hours from schedule where schedule.coach_id = 2), 2);
+
+VALUES (3, 6, 40, 1200 * (select duration_hours from schedule where schedule.coach_id = 3), 1),
+ (3, 6, 36, 1200 * (select duration_hours from schedule where schedule.coach_id = 3), 1),
+ (3, 6, 33, 1200 * (select duration_hours from schedule where schedule.coach_id = 3), 1),
+ (3, 6, 32, 1200 * (select duration_hours from schedule where schedule.coach_id = 3), 1),
+ (3, 6, 30, 1200 * (select duration_hours from schedule where schedule.coach_id = 3), 1),
+ (3, 6, 29, 1200 * (select duration_hours from schedule where schedule.coach_id = 3), 1),
+ (3, 6, 27, 1200 * (select duration_hours from schedule where schedule.coach_id = 3), 1),
+ (3, 6, 25, 1200 * (select duration_hours from schedule where schedule.coach_id = 3), 1),
+ (3, 6, 21, 1200 * (select duration_hours from schedule where schedule.coach_id = 3), 1);
+
+VALUES (4, 2, 23, 1500 * (SELECT duration_hours FROM schedule WHERE schedule.coach_id = 4), 4),
+       (6, 2, 26, 1100 * (SELECT duration_hours FROM schedule WHERE schedule.coach_id = 4), 6);
+
+VALUES (5,5,21,1000 * (SELECT duration_hours FROM schedule WHERE schedule.coach_id = 4),5),
+ (5,5,27,1000 * (SELECT duration_hours FROM schedule WHERE schedule.coach_id = 4),5),
+ (5,5,29,1000 * (SELECT duration_hours FROM schedule WHERE schedule.coach_id = 4),5);
